c47221e8a10585bd941df85a9e7ca83c7d99dae3
sergeavila7 <serge_avila7@yahoo.com>
2025-01-06 16:16:06 -0600

    feat: add quantity and total

--- app/dishForm.tsx
+++ app/dishForm.tsx
@@ -1,25 +1,28 @@
-import React, { useContext, useState } from "react";
-import { StyleSheet } from "react-native";
-import { router } from "expo-router";
+import React, { useContext, useEffect, useState } from "react";
 import { Plus, Minus } from "lucide-react-native";
 
 import OrdersContext from "@/context/orders/ordersContext";
 import { Box } from "@/components/ui/box";
-import { Text } from "@/components/ui/text";
-import globalStyles from "@/styles/global";
 import { HStack } from "@/components/ui/hstack";
 import { Button } from "@/components/ui";
 import { ButtonIcon } from "@/components/ui/button";
 import { Center } from "@/components/ui/center";
 import { Heading } from "@/components/ui/heading";
 import { Input, InputField } from "@/components/ui/input";
+import { Text } from "@/components/ui/text";
 
 export default function DishForm() {
-  const ordersContext = useContext(OrdersContext);
+  const {
+    state: { dish },
+  } = useContext(OrdersContext);
+  const { price } = dish;
 
-  const { dishName, image, description, price } = ordersContext.state.dish;
+  const [quantity, setQuantity] = useState(0);
+  const [total, setTotal] = useState(0);
 
-  const [quantity, setQuantity] = useState("");
+  useEffect(() => {
+    setTotal(price * quantity);
+  }, [quantity, price]);
 
   return (
     <Center className="my-5">
@@ -28,7 +31,7 @@ export default function DishForm() {
       </Heading>
       <HStack reversed space="lg" className="mt-5">
         <Box className="h-25 w-40">
-          <Button>
+          <Button onPress={() => setQuantity(quantity + 1)}>
             <ButtonIcon as={Plus} />
           </Button>
         </Box>
@@ -37,17 +40,22 @@ export default function DishForm() {
             <InputField
               className="text-center"
               value={quantity.toString()}
-              onChangeText={(quantity) => setQuantity(quantity)}
+              onChangeText={(value) => setQuantity(parseInt(value, 10) || 0)}
               keyboardType="numeric"
             />
           </Input>
         </Box>
         <Box className="h-25 w-40">
-          <Button>
+          <Button onPress={() => setQuantity(Math.max(quantity - 1, 0))}>
             <ButtonIcon as={Minus} />
           </Button>
         </Box>
       </HStack>
+      <Box className="mt-5">
+        <Text className="text-slate-950" bold>
+          Subtotal: ${total}
+        </Text>
+      </Box>
     </Center>
   );
 }
