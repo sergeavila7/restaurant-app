1412e53d96644f11350b157a7bf21b02c65a6e4c
sergeavila7 <serge_avila7@yahoo.com>
2025-01-06 15:59:22 -0600

    feat: add form components

--- app/dishForm.tsx
+++ app/dishForm.tsx
@@ -1,10 +1,53 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useContext, useState } from "react";
+import { StyleSheet } from "react-native";
+import { router } from "expo-router";
+import { Plus, Minus } from "lucide-react-native";
+
+import OrdersContext from "@/context/orders/ordersContext";
+import { Box } from "@/components/ui/box";
+import { Text } from "@/components/ui/text";
+import globalStyles from "@/styles/global";
+import { HStack } from "@/components/ui/hstack";
+import { Button } from "@/components/ui";
+import { ButtonIcon } from "@/components/ui/button";
+import { Center } from "@/components/ui/center";
+import { Heading } from "@/components/ui/heading";
+import { Input, InputField } from "@/components/ui/input";
 
 export default function DishForm() {
+  const ordersContext = useContext(OrdersContext);
+
+  const { dishName, image, description, price } = ordersContext.state.dish;
+
+  const [quantity, setQuantity] = useState("");
+
   return (
-    <View>
-      <Text>DishForm</Text>
-    </View>
+    <Center className="my-5">
+      <Heading className="text-center" size="xl" bold>
+        Cantidad
+      </Heading>
+      <HStack reversed space="lg" className="mt-5">
+        <Box className="h-25 w-40">
+          <Button>
+            <ButtonIcon as={Plus} />
+          </Button>
+        </Box>
+        <Box className="h-25 w-20">
+          <Input>
+            <InputField
+              className="text-center"
+              value={quantity.toString()}
+              onChangeText={(quantity) => setQuantity(quantity)}
+              keyboardType="numeric"
+            />
+          </Input>
+        </Box>
+        <Box className="h-25 w-40">
+          <Button>
+            <ButtonIcon as={Minus} />
+          </Button>
+        </Box>
+      </HStack>
+    </Center>
   );
 }
